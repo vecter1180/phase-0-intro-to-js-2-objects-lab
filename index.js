b70e58cc13dec3663534f3a83e2b09f91cf7543c
Brian Allen <vecter1180@gmail.com>
2022-06-20 02:22:50 -0400

    commit tree f46f5b164d33cc3d6fa829bf2975c98fdb8106f1

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your solution in this file!
+const employee = {
+    name:"Gus",
+    address: "123 fake steet"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return  Object.keys(updatedEmployeeWithKeyAndValue)
+}
